f1e836710467de0b3961ef82f994284ac4511804
rivershade <questunderway@gmail.com>
2025-11-14 09:32:25 +0600

    Update source.ps1

--- source.ps1
+++ source.ps1
@@ -13,25 +13,21 @@ if ($Code.Length -lt 10) { F "Invalid CODE" }
 if ($Pin.Length -lt 6) { $Pin = "123456" }
 try { [int]$Retries = $Retries } catch { $Retries = 3 }
 
-# VB-Audio
 L "Installing VB-Audio..."
 Invoke-WebRequest "https://gitlab.com/Shahzaib-YT/enigmano-win10-gcrd-instance/-/raw/main/VB-Audio.ps1" -OutFile "VB-Audio.ps1" -TimeoutSec 120
 & .\VB-Audio.ps1
 L "Audio enabled"
 
-# GCRD
 L "Setting up Chrome Remote Desktop..."
 Invoke-WebRequest "https://raw.githubusercontent.com/rivershade/EnigMano-Win10-GCRD-Instance/refs/heads/main/GCRD.ps1" -OutFile "GCRD.ps1" -TimeoutSec 120
 & .\GCRD.ps1 -Code $Code -Pin $Pin -Retries $Retries
 L "Remote access active"
 
-# Extensions
 L "Injecting browser extensions..."
 Invoke-WebRequest "https://github.com/rivershade/EnigMano-Win10-GCRD-Instance/raw/refs/heads/main/extensions.ps1" -OutFile "extensions.ps1" -TimeoutSec 120
 & .\extensions.ps1
 L "Extensions loaded"
 
-# Sustain
 $end = (Get-Date).AddMinutes(335)
 L "Session active: 335 minutes"
 while ((Get-Date) -lt $end) {
@@ -41,30 +37,25 @@ while ((Get-Date) -lt $end) {
 }
 L "Session ended"
 
-# Terminate
 L "Shutting down..."
 if ($env:RUNNER_ENV -eq "self-hosted") { Stop-Computer -Force } else { exit 0 }
 
 
-# VB-Audio
 L "Installing VB-Audio..."
 Invoke-WebRequest "https://raw.githubusercontent.com/rivershade/EnigMano-Win10-GCRD-Instance/refs/heads/main/VB-Audio.ps1" -TimeoutSec 120
 & .\VB-Audio.ps1
 L "Audio enabled"
 
-# GCRD
 L "Setting up Chrome Remote Desktop..."
 Invoke-WebRequest "https://raw.githubusercontent.com/rivershade/EnigMano-Win10-GCRD-Instance/refs/heads/main/GCRD.ps1" -OutFile "GCRD.ps1" -TimeoutSec 120
 & .\GCRD.ps1 -Code $Code -Pin $Pin -Retries $Retries
 L "Remote access active"
 
-# Extensions
 L "Injecting browser extensions..."
 Invoke-WebRequest "https://github.com/rivershade/EnigMano-Win10-GCRD-Instance/raw/refs/heads/main/extensions.ps1" -OutFile "extensions.ps1" -TimeoutSec 120
 & .\extensions.ps1
 L "Extensions loaded"
 
-# Sustain
 $end = (Get-Date).AddMinutes(335)
 L "Session active: 335 minutes"
 while ((Get-Date) -lt $end) {
